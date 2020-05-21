7549a65e415d893c15ba5a4689ef3a2a3500a5c4
Ilya Rakipov <ilya.rakipov@gmail.com>
2020-05-21 21:10:39 +0500

    Update main.cpp
    change file

--- main.cpp
+++ main.cpp
@@ -2,132 +2,250 @@
 
 #include <stdlib.h> 
 
+#include <math.h> 
 
 
-int slau(int n, const double** a, const double* b, double* x);
+
+int slau(int n, double** a, double* x);
+
+double pol(int n, double x, double* c);
+
+int lms(int n, int p, double* x, double* y, double* c);
 
 
 
 int main(int argc, char* argv[]) {
 
-    int n, i, j;
-    double check;
+    int i, n, p;
+
+    double dx;
+
     FILE* file;
 
-    file = fopen("first.txt", "r");
+    file = fopen("vector.txt", "r");
+
     fscanf(file, "%i", &n);
 
-    double** a = (double**)malloc(n * sizeof(double*));
-    double* b = (double*)malloc(n * sizeof(double));
+    fscanf(file, "%i", &p);
+
     double* x = (double*)malloc(n * sizeof(double));
 
+    double* y = (double*)malloc(n * sizeof(double));
+
+    double* c = (double*)malloc((p + 1) * sizeof(double));
+
+    printf("x ");
+
     for (i = 0; i < n; i++) {
-        a[i] = (double*)malloc(n * sizeof(double));
-        for (j = 0; j < n; j++) {
-            fscanf(file, "%lf", &a[i][j]);
-            printf("%12.2fx%i", a[i][j], j + 1);
-        }
-        fscanf(file, "%lf", &b[i]);
-        printf("%12.2f \n", b[i]);
+
+        fscanf(file, "%lf", &x[i]);
+
+        printf("%5.2lf ", x[i]);
+
     }
 
-    putchar('\n');
+    printf("\ny ");
 
-    if (slau(n, a, b, x)) {
-        printf("\nThe roots of the system:\n");
-        for (i = 0; i < n; i++)
-            printf("  x%i = %.3lf \n", i + 1, x[i]);
-        printf("\nCheck:\n");
-        for (i = 0; i < n; i++) {
-            check = 0.;
-            for (j = 0; j < n; j++) {
-                check += a[i][j] * x[j];
-                printf("%7.2lf*%.2lf ", a[i][j], x[j]);
-                if (j < (n - 1))
-                    printf(" +");
-                else
-                    printf(" =");
-            }
-            printf("  %.2lf\n", check);
+    for (i = 0; i < n; i++) {
+
+        fscanf(file, "%lf", &y[i]);
+
+        printf("%5.2lf ", y[i]);
+
+    }
+
+
+
+    if (lms(n, p, x, y, c)) {
+
+        dx = x[0];
+
+        while (dx <= x[n - 1]) {
+
+            printf("\n%1.1lf %lf", dx, pol(n, dx, c));
+
+            dx += 0.1;
+
         }
-    } else printf("The system cannot be solved by this method!\n");
 
-    for (i = 0; i < n; i++)
-        free(a[i]);
+    }
 
-    free(a);
-    free(b);
     free(x);
 
+    free(y);
+
     return 0;
+
 }
 
 
 
-int slau(int n, const double** a, const double* b, double* x) {
+int slau(int n, double** a, double* x) {
 
     int i, j, k;
-    double tmp;
-    double** LU, * y;
 
-    LU = (double**)malloc(n * sizeof(double*));
+    double** A, tmp;
 
-    for (i = 0; i < n; i++)
-        LU[i] = (double*)malloc(n * sizeof(double));
-
-    y = (double*)malloc(n * sizeof(double));
+    A = (double**)malloc((n + 1) * sizeof(double*));
 
     for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
-            tmp = 0.;
-            if (j > i) {
-                for (k = 0; k < i; k++)
-                    tmp += LU[i][k] * LU[k][j];
-                LU[i][j] = (a[i][j] - tmp) / LU[i][i];
-            }
-            else {
-                for (k = 0; k < j; k++)
-                    tmp += LU[i][k] * LU[k][j];
-                LU[i][j] = a[i][j] - tmp;
 
-                if (LU[i][j] == 0. && i == j) {
-                    for (k = 0; k < n; k++) {
-                        free(LU[k]);
-                    }
+        A[i] = (double*)malloc(n * sizeof(double));
 
-                    free(LU);
-                    free(y);
+        for (j = 0; j <= n; j++) {
 
-                    return 0;
-                }
-            }
+            A[i][j] = a[i][j];
+
         }
+
     }
 
     for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++)
-            printf("%12.4lf\t", LU[i][j]);
-        putchar('\n');
+
+        tmp = A[i][i];
+
+        if (fabs(tmp) < 1e-6) {
+
+            for (j = 0; j <= n; j++)
+
+                free(A[j]);
+
+            free(A);
+
+            return 0;
+
+        }
+
+        for (j = i; j <= n; j++) {
+
+            A[i][j] /= tmp;
+
+        }
+
+        for (j = 0; j < n; j++) {
+
+            if (j == i)
+
+                continue;
+
+            tmp = A[j][i];
+
+            for (k = 0; k <= n; k++)
+
+                A[j][k] -= tmp * A[i][k];
+
+        }
+
     }
 
     for (i = 0; i < n; i++) {
-        tmp = 0.;
-        for (k = 0; k < i; k++)
-            tmp += LU[i][k] * y[k];
-        y[i] = (b[i] - tmp) / LU[i][i];
+
+        x[i] = A[i][n];
+
+        free(A[i]);
+
     }
 
-    for (i = n - 1; i >= 0; i--) {
-        tmp = 0.;
-        for (k = i + 1; k < n; k++)
-            tmp += LU[i][k] * x[k];
-        x[i] = y[i] - tmp;
-    }
-
-    for (i = 0; i < n; i++) free(LU[i]);
-
-    free(LU);
-    free(y);
+    free(A);
 
     return 1;
+
+}
+
+
+
+double pol(int n, double x, double* c) {
+
+    double y;
+
+    int i;
+
+    y = c[n];
+
+    for (i = n; i > 0; i--)
+
+        y = y * x + c[i - 1];
+
+    return y;
+
+}
+
+
+
+int lms(int n, int p, double* x, double* y, double* c) {
+
+    double** a;
+
+    int i, j, k, iret;
+
+    a = (double**)malloc((p + 2) * sizeof(double*));
+
+    for (i = 0; i <= p; i++)
+
+        a[i] = (double*)malloc((p + 1) * sizeof(double));
+
+    for (i = 0; i <= p; i++) {
+
+        a[0][i] = 0.;
+
+        for (k = 0; k < n; k++)
+
+            a[0][i] += pow(x[k], i);
+
+        k = 0;
+
+        for (j = i - 1; j >= 0; j--) {
+
+            k++;
+
+            a[k][j] = a[0][i];
+
+        }
+
+    }
+
+    for (j = 1; j <= p; j++) {
+
+        a[j][p] = 0.;
+
+        for (k = 0; k < n; k++)
+
+            a[j][p] += pow(x[k], p + j);
+
+        i = p;
+
+        for (k = j + 1; k <= p; k++) {
+
+            i--;
+
+            a[k][i] = a[j][p];
+
+        }
+
+    }
+
+    for (i = 0; i <= p; i++) {
+
+        a[i][p + 1] = 0.;
+
+        for (j = 0; j < n; j++)
+
+            a[i][p + 1] += y[j] * pow(x[j], i);
+
+    }
+
+    iret = slau(p + 1, a, c);
+
+    for (i = 0; i <= p; i++)
+
+        printf("\nC[%i] = %lf", i, c[i]);
+
+    for (i = 0; i <= p; i++)
+
+        free(a[i]);
+
+    free(a);
+
+    return iret;
+
 }
